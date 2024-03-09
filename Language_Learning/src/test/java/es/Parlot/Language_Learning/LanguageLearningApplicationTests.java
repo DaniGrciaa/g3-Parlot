680289c9f900138889c28caf774da95734e793e4
DaniGarcia <dgarciaestebanez@gmail.com>
2024-03-09 15:13:32 +0100

    Initial commit

---
+++ Language_Learning/src/test/java/es/Parlot/Language_Learning/LanguageLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package es.Parlot.Language_Learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguageLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
